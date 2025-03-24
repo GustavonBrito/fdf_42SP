59989465f0919d844d2a3372446f58b9083ca893
GustavonBrito <gustavoserafio@gmail.com>
2025-03-24 06:09:30 -0300

    chore: mounting project struct

---
+++ open_x.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   open_x.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 12:43:59 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2025/03/23 20:55:54 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx/mlx.h"
+#include "minilibx.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+	
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*destiny;
+
+	destiny = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)destiny = color;
+}
+
+int	main(void)
+{
+	int		x;
+	int		y;
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	y = 0;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 500, 500, "fdf");
+	img.img = mlx_new_image(mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	while (y <= 50)
+	{
+		x = 0;
+		while (x <= 50)
+		{		
+			my_mlx_pixel_put(&img, 200 + x, 200 + y, 0x0000ff);
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	mlx_destroy_display(mlx);
+	//free(mlx);
+}
