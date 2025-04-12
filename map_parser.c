03c56cbabc0e8f57ea1a2a1d65d932dbded447bc
Gustavo Serafio Correa De Brito <gserafio@c1r7p2.42sp.org.br>
2025-04-12 04:39:33 -0300

    feat: map_parser in running

--- map_parser.c
+++ map_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 23:31:29 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/04/10 01:18:21 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/12 04:36:46 by gserafio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,88 @@
 #include "libft/headers/ft_printf.h"
 #include "libft/headers/get_next_line.h"
 #include "libft/headers/libft.h"
-#include "minilibx/mlx.h"
 
-int save_values_to_struct(char *path_file)
+///Objetivo: Abre o arquivo, aloca o mapa e preenche os dados usando parse_line_to_row.
+// Depois tenho que fazer o free de map->coordinates, map->coordinates[j] e map;
+t_map *read_and_alloc_map(int fd)
 {
 	char *buffer;
 	char **split;
-	int fd;
-	int count;
-	
-	fd = 0;
-	count = 0;
-	//t_point *map_coord;
-	fd = open(path_file,O_RDONLY);
+	int i;
+	int y;
+	int j;
+	t_map *map = NULL;
+
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (NULL);
+	y = 0;
+	j = 0;
 	while (1)
 	{
 		buffer = get_next_line(fd);
 		if (buffer == NULL)
-			break;
+			break ;
 		split = ft_split(buffer, ' ');
-		while (*split)
-		{
-			count++;
-			split++;
-		}
+		i = 0;
+		while(split[i])
+			i++;
+		ft_free_split(split);
+		free(buffer);
+		y++;
 	}
-	ft_printf("Count to malloc: %d\n", count);
+	map->max_x = i - 1;
+	map->max_y = y;
+	map->coordinates = malloc(sizeof(t_point *) * map->max_y);
+	if (!map->coordinates)
+		return (NULL);
+	i = 0;
+	while (i < map->max_y)
+	{
+		map->coordinates[i] = malloc(sizeof(t_point) * map->max_x);
+		if (!map->coordinates[i])
+		{
+			while (j <= i)
+			{
+				free(map->coordinates[j]);
+				j++;
+			}
+			return (NULL);
+		}
+		i++;
+	}
+	return (map);
+}
+
+// /Objetivo: Lê uma linha do mapa (ex: "1 0 0 -1 -1 0 ...") e armazena os valores em um array de t_point, setando também a coordenada y para cada ponto da linha.
+//char *line,t_point *struct, int y
+int parse_line_to_row(t_map *map, int fd)
+{
+	char *buffer;
+	
+	buffer = get_next_line(fd);
+	
+}
+
+//Objetivo: Ajusta todas as coordenadas x e y para que o centro do mapa fique na origem (0, 0). Isso melhora a projeção depois.
+//t_map center_map(t_map *map, int width, int height);
+
+
+//Objetivo: Converte os pontos do mapa para o sistema de projeção desejado (ex: isométrico), considerando os valores de z (profundidade).
+//t_map apply_projection(t_map *map, int width, int height);
+
+int init_parser(char *file_path)
+{
+	t_map *map;
+	int fd;
+	
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	map = read_and_alloc_map(fd);
+	if (map == NULL)
+		return (close_and_return_err(fd));
+	if (parse_line_to_row(map, fd) == -1)
+		return (close_and_return_err(fd));
 	return (1);
 }
-
-int init_parser(char *path_file)
-{
-	save_values_to_struct(path_file);
-	return (0);
-}
-
