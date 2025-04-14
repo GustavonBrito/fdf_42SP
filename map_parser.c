46cd3f8ae52fa5e3686e7487146fa5f55358215b
GustavonBrito <gustavoserafio@gmail.com>
2025-04-14 02:18:02 -0300

    feat: implemented parser and utils parser

--- map_parser.c
+++ map_parser.c
@@ -3,32 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   map_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 23:31:29 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/04/13 17:13:33 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/04/14 01:58:59 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/messages.h"
 #include "includes/fdf.h"
+#include "includes/messages.h"
 #include "libft/headers/ft_printf.h"
 #include "libft/headers/get_next_line.h"
 #include "libft/headers/libft.h"
 
-///Objetivo: Abre o arquivo, aloca o mapa e preenche os dados usando parse_line_to_row.
 // Depois tenho que fazer o free de map->coordinates, map->coordinates[j] e map;
 
-static void	populate_pts(t_map *map);
-void		read_and_alloc_map(int fd, t_map *map);
-int			parse_line_to_row(t_map *map, char *file_path);
-t_map		*init_parser(char *file_path);
+void	read_and_alloc_map(int fd, t_map *map);
+int		parse_line_to_row(t_map *map, char *file_path);
+t_map	*center_map(t_map *map);
+t_map	*init_parser(char *file_path);
 
 t_map	*init_parser(char *file_path)
 {
-	t_map *map;
-	int fd;
-	
+	t_map	*map;
+	int		fd;
+
 	map = (t_map *)malloc(sizeof(t_map));
 	if (!map)
 		return (NULL);
@@ -36,68 +35,44 @@ t_map	*init_parser(char *file_path)
 	if (fd < 0)
 		return (NULL);
 	read_and_alloc_map(fd, map);
-	if (map == NULL)
-		return (close_and_return_err(fd), NULL);
+	if (!map)
+		return (close(fd), NULL);
 	if (parse_line_to_row(map, file_path) == -1)
 		return (NULL);
-	// map = center_map(map,map->max_x, map->max_y);
-	if (map == NULL)
+	map = center_map(map);
+	if (!map)
+		return (NULL);
+	map = apply_projection(map);
+	if (!map)
 		return (NULL);
 	return (map);
 }
 
-static void populate_pts(t_map *map)
-{
-	int		x;
-	int		i;
-	
-	x = -1;
-	i = -1;
-	// add (t_point **) to prevent undefined behavior
-	map->coordinates = (t_point **)malloc(sizeof(t_point *) * map->max_y);
-	if (!map->coordinates)
-		return ;
-	while (++x < map->max_y)
-	{
-		map->coordinates[x] = malloc(sizeof(t_point) * map->max_x);
-		if (!map->coordinates[x])
-		{
-			while (++i <= x)
-				free(map->coordinates[i]);
-			return ;
-		}
-	}
-}
-
 void	read_and_alloc_map(int fd, t_map *map)
 {
-	char *buffer;
-	char **split;
-	int x;
-	int y;
-	
+	char	*buffer;
+	char	**split;
+	int		x;
+	int		y;
+
 	y = -1;
+	x = 0;
 	while (++y != -1)
 	{
 		buffer = get_next_line(fd);
 		if (!buffer)
 			break ;
-		x = 0;
 		split = ft_split(buffer, ' ');
-		while(split[x])
+		while (split[x] && split[x][0] != '\n')
 			x++;
 		ft_free_split(split, buffer);
 	}
 	map->max_x = x;
 	map->max_y = y;
-	ft_printf("X: %d\n", map->max_x);
-	ft_printf("Y: %d\n", map->max_y);
 	populate_pts(map);
 }
 
-// /Objetivo: Lê uma linha do mapa (ex: "1 0 0 -1 -1 0 ...") e armazena os valores em um array de t_point, setando também a coordenada y para cada ponto da linha.
-//char *line,t_point *struct, int y
-int parse_line_to_row(t_map *map, char *file_path)
+int	parse_line_to_row(t_map *map, char *file_path)
 {
 	char	**split;
 	char	*buffer;
@@ -107,12 +82,14 @@ int parse_line_to_row(t_map *map, char *file_path)
 
 	y = -1;
 	fd = open(file_path, O_RDONLY);
+	map->max_z = 0;
+	map->min_z = 0;
 	while (++y < map->max_y)
 	{
 		buffer = get_next_line(fd);
 		split = ft_split(buffer, ' ');
 		x = -1;
-		while(++x < map->max_x)
+		while (++x < map->max_x)
 		{
 			if (verify_hex(split[x]) == IS_HEX)
 				parse_hex_to_map(split[x], map, y, x);
@@ -120,22 +97,68 @@ int parse_line_to_row(t_map *map, char *file_path)
 			{
 				map->coordinates[y][x].z = ft_atoi(split[x]);
 				map->coordinates[y][x].rgb = 0;
+				if (ft_atoi(split[x]) > map->max_z)
+					map->max_z = ft_atoi(split[x]);
+				if (ft_atoi(split[x]) < map->min_z)
+					map->min_z = ft_atoi(split[x]);
 			}
 			map->coordinates[y][x].x = x;
 			map->coordinates[y][x].y = y;
 		}
+		ft_free_split(split, buffer);
 	}
 	close(fd);
 	return (1);
 }
 
-//Objetivo: Ajusta todas as coordenadas x e y para que o centro do mapa fique na origem (0, 0). Isso melhora a projeção depois.
-// t_map *center_map(t_map *map, int width, int height)
+t_map	*center_map(t_map *map)
+{
+	int	center_x;
+	int	center_y;
+	int	x;
+	int	y;
+
+	center_x = map->max_x / 2;
+	center_y = map->max_y / 2;
+	y = -1;
+	while (++y < map->max_y)
+	{
+		x = -1;
+		while (++x < map->max_x)
+		{
+			map->coordinates[y][x].x = x - center_x;
+			map->coordinates[y][x].y = y - center_y;
+		}
+	}
+	return (map);
+}
+
+// t_map	*apply_projection(t_map *map)
 // {
-	
+// 	int		x;	
+// 	int		y;
+// 	int		original_x;
+// 	int		original_y;
+// 	float	z_multiplier;
+// 	int 	z_range;
+
+// 	z_range = map->max_z + abs(map->min_z);
+// 	y = -1;
+// 	if (z_range > 50)
+// 		z_multiplier = 1;
+// 	else
+// 		z_multiplier = 8;
+// 	while (++y < map->max_y)
+// 	{
+// 		x = -1;
+// 		while (++x < map->max_x)
+// 		{
+// 			original_x = map->coordinates[y][x].x;
+// 			original_y = map->coordinates[y][x].y;
+// 			map->coordinates[y][x].x = (original_x - original_y) * cos(ISO_ANGLE);
+// 			map->coordinates[y][x].y = ((original_x + original_y) * sin(ISO_ANGLE))
+// 				- (map->coordinates[y][x].z * z_multiplier);
+// 		}
+// 	}
 // 	return (map);
 // }
-
-//Objetivo: Converte os pontos do mapa para o sistema de projeção desejado (ex: isométrico), considerando os valores de z (profundidade).
-//t_map apply_projection(t_map *map, int width, int height);
-
--- utils_map_parser.c
+++ utils_map_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_map_parser.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 02:31:49 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/13 16:32:38 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/04/13 22:51:52 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 #include "libft/headers/ft_printf.h"
 #include "libft/headers/libft.h"
 
-void	close_and_return_err(int fd)
-{
-	close(fd);
-}
+void 		populate_pts(t_map *map);
+void		parse_hex_to_map(char *buffer, t_map *map, int y, int x);
+int			verify_hex(char *token);
 
 int	verify_hex(char *token)
 {
@@ -42,10 +41,39 @@ int	verify_hex(char *token)
 // Ficar de olho aonde eu vou liberar essa memoria depois de utilizar
 void	parse_hex_to_map(char *buffer, t_map *map, int y, int x)
 {
+	int		z_from_map;
 	char	**split;
 
 	split = ft_split(buffer, ',');
-	map->coordinates[y][x].z = ft_atoi(split[0]);
+	z_from_map = ft_atoi(split[0]);
+	map->coordinates[y][x].z = z_from_map;
 	map->coordinates[y][x].rgb = ft_atoi_hexa(&split[1][2]);
+	if (z_from_map > map->max_z)
+		map->max_z = z_from_map;
+	if (z_from_map < map->min_z)
+		map->min_z = z_from_map;
 	ft_free_split(split, NULL);
+}
+
+void populate_pts(t_map *map)
+{
+	int		x;
+	int		i;
+	
+	x = -1;
+	i = -1;
+	// add (t_point **) to prevent undefined behavior
+	map->coordinates = (t_point **)malloc(sizeof(t_point *) * map->max_y);
+	if (!map->coordinates)
+		return ;
+	while (++x < map->max_y)
+	{
+		map->coordinates[x] = malloc(sizeof(t_point) * map->max_x);
+		if (!map->coordinates[x])
+		{
+			while (++i <= x)
+				free(map->coordinates[i]);
+			return ;
+		}
+	}
 }
