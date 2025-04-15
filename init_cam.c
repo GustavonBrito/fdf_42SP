8cb46790767a9f09c08997814cdfced362a6af25
GustavonBrito <gustavoserafio@gmail.com>
2025-04-15 02:46:35 -0300

    feat: cam made

--- init_cam.c
+++ init_cam.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   init_cam.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:02:50 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/14 21:34:46 by gserafio         ###   ########.fr       */
+/*   Updated: 2025/04/15 02:32:05 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/fdf.h"
+#include "libft/headers/ft_printf.h"
 
 t_cam *init_cam(t_map *map)
 {
@@ -19,8 +20,8 @@ t_cam *init_cam(t_map *map)
 	cam = (t_cam *)malloc(sizeof(t_cam));
 	if (!cam)
 		return (NULL);
-	cam->offset_x = WIDTH / 2;
-	cam->offset_y = HEIGHT / 2;
+	cam->offset_x = WIDTH / 2.0;
+	cam->offset_y = HEIGHT / 2.0;
 	cam->scale_z = 1;
 	cam->scale_factor = get_scale(map);
 	return (cam);
