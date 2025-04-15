dd41eb2e99cc3314ff3d8a1ca64546a1af5b84bc
GustavonBrito <gustavoserafio@gmail.com>
2025-04-15 02:47:40 -0300

    feat: initialized window

--- init_mlx.c
+++ init_mlx.c
@@ -3,19 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   init_mlx.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:03:29 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/14 22:08:11 by gserafio         ###   ########.fr       */
+/*   Updated: 2025/04/15 02:44:45 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <includes/fdf.h>
+#include "includes/fdf.h"
+#include "minilibx/mlx.h"
+#include "libft/headers/ft_printf.h"
+#include <signal.h>
+
+// int	mouse_hook(int keycode, t_mlx *mlx)
+// {
+// 	(void) mlx;
+	
+// 	ft_printf("%d", keycode);
+// 	ft_printf("Hello from key_hook!\n");
+// 	return (0);
+// }
+
+int	key_hook(int keycode, t_mlx *mlx)
+{
+	(void) mlx;
+	if (keycode == 65307)
+	{
+		kill(getpid(), SIGTERM);
+	}
+	return (0);
+}
 
 int init_mlx(t_mlx *mlx)
 {
 	mlx->mlx = mlx_init();
 	if (!mlx->mlx)
 		return (0);
+	mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "fdf_42");
+	mlx->img = mlx_new_image(mlx, mlx->cam->offset_x, mlx->cam->offset_y);
+	mlx_key_hook(mlx->win, key_hook, mlx);
+	//mlx_mouse_hook(mlx->win, mouse_hook, mlx);
+	mlx_loop(mlx->mlx);
 	return (1);
 }
