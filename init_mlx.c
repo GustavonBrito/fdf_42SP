8c1fdb9e86e7ded86a64b0a93c8ebda765b4f79d
GustavonBrito <gustavoserafio@gmail.com>
2025-04-16 03:48:57 -0300

    feat: implemented close window on X

--- init_mlx.c
+++ init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:03:29 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/15 13:23:35 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/16 03:48:19 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 #include "minilibx/mlx.h"
 #include "libft/headers/ft_printf.h"
 #include <signal.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
 
-// int	mouse_hook(int keycode, t_mlx *mlx)
-// {
-// 	(void) mlx;
-	
-// 	ft_printf("%d", keycode);
-// 	ft_printf("Hello from key_hook!\n");
-// 	return (0);
-// }
+int close_window()
+{
+	exit(0);
+	return (0);
+}
+
+int	mouse_hook(int keycode, t_mlx *mlx)
+{
+	(void) mlx;
+	ft_printf("Keycode %d\n", keycode);
+	if (keycode == 17)
+		ft_printf("Hello from key_hook!\n");
+	return (0);
+}
 
 int	key_hook(int keycode, t_mlx *mlx)
 {
@@ -51,9 +59,10 @@ int init_mlx(t_mlx *mlx)
 	if (!mlx->mlx)
 		return (0);
 	mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "fdf_42");
-	//mlx->img = mlx_new_image(mlx, mlx->cam->offset_x, mlx->cam->offset_y);
+	//mlx->img->img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	mlx_key_hook(mlx->win, key_hook, mlx);
-	//mlx_mouse_hook(mlx->win, mouse_hook, mlx);
+	mlx_mouse_hook(mlx->win, mouse_hook, mlx);
+	mlx_hook(mlx->win, 17, 0, close_window, mlx);
 	mlx_loop(mlx->mlx);
 	return (1);
 }
