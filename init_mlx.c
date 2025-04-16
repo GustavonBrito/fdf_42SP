96ca49c871b4c54d74e83b89eb9285f20b4cb1ff
GustavonBrito <gustavoserafio@gmail.com>
2025-04-16 03:57:18 -0300

    feat: init mlx has to implement free on all previous mallocs

--- init_mlx.c
+++ init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:03:29 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/16 03:53:04 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/16 03:56:26 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,16 @@ int close_window()
 	return (0);
 }
 
-int	mouse_hook(int keycode, t_mlx *mlx)
-{
-	(void) mlx;
-	ft_printf("Keycode %d\n", keycode);
-	if (keycode == 17)
-		ft_printf("Hello from key_hook!\n");
-	return (0);
-}
-
 int	key_hook(int keycode, t_mlx *mlx)
 {
 	int y;
 	
 	if (keycode == 65307)
 	{
-		free(mlx->map->coordinates);
 		y = -1;
 		while(++y < mlx->map->max_y)
-		{
 			free(mlx->map->coordinates[y]);
-		}
+		free(mlx->map->coordinates);
 		mlx_destroy_window(mlx->mlx, mlx->win);
 		free(mlx->cam);
 		free(mlx->map);
@@ -61,7 +50,6 @@ int init_mlx(t_mlx *mlx)
 	mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "fdf_42");
 	//mlx->img->img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	mlx_key_hook(mlx->win, key_hook, mlx);
-	mlx_mouse_hook(mlx->win, mouse_hook, mlx);
 	mlx_hook(mlx->win, 17, 0, close_window, mlx);
 	mlx_loop(mlx->mlx);
 	mlx_destroy_display(mlx);
