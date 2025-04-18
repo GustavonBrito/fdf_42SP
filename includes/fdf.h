1444c3c3a6600db8676804d8a56aaa4769081d09
GustavonBrito <gustavoserafio@gmail.com>
2025-04-18 19:47:17 -0300

    feat: added headers adapted on norm

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:32:10 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/18 17:52:41 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/18 19:21:11 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # define WIDTH 1400
 # define HEIGHT 900
 # define RED 16711680
+# define IS_HEX 16
 
 typedef struct s_bres
 {
@@ -102,5 +103,6 @@ int			close_by_x(t_mlx *mlx);
 int			close_by_esc(int keycode, t_mlx *mlx);
 void		close_window(t_mlx *mlx);
 void		bresenham(t_mlx *mlx, t_line *line);
+void		error_in_step(int error_code);
 
 #endif
--- includes/messages.h
+++ includes/messages.h
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   messages.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gserafio <gserafio@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:32:17 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/17 12:32:18 by gserafio         ###   ########.fr       */
+/*   Updated: 2025/04/18 19:46:32 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MESSAGES
-#define MESSAGES
+#ifndef MESSAGES_H
+# define MESSAGES_H
 
-	#define ERR_USAGE "Usage: ./fdf fdf.42"
-	#define ERR_MAP "Incorrect: MAP STRUCT"
-	#define IS_HEX 16
+# define ERR_USAGE "Usage: ./fdf fdf.42"
+# define ERR_MAP "Incorrect: MAP STRUCT"
+# define ERR_VALIDATION 1
+# define ERR_PARSE 2
+# define ERR_CAM 3
+# define ERR_MLX 4
 
 #endif
