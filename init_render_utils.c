d4758da10768fc711dc19506928aea0d04bbea10
GustavonBrito <gustavoserafio@gmail.com>
2025-04-18 18:51:21 -0300

    feat: applied bresenham to utils to render image

--- init_render_utils.c
+++ init_render_utils.c
@@ -6,7 +6,7 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:30:47 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/18 12:40:12 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/18 18:50:48 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,21 +28,21 @@ void	apply_isometric(t_line *line)
 	line->start.x = new_start.x;
 	line->start.y = new_start.y;
 	new_end.x = (line->end.x - line->end.y) * cos(ISO_ANGLE);
-	new_end.y = (line->end.x + line->end.y) * sin(ISO_ANGLE)
-		- line->end.z;
+	new_end.y = (line->end.x + line->end.y) * sin(ISO_ANGLE) - line->end.z;
 	line->end.x = new_end.x;
 	line->end.y = new_end.y;
 }
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	int pixel = (y * data->line_length) + (x * 4);
+	int	pixel;
 
+	pixel = (y * data->line_length) + (x * 4);
 	if (data->endian == 1)
 	{
-		data->addr[pixel + 1] = (color >> 16) & 0xFF;// Bit Red
-		data->addr[pixel + 2] = (color >> 8) & 0xFF; // Bit Green
-		data->addr[pixel + 3] = (color) & 0xFF; // Bit Blue
+		data->addr[pixel + 1] = (color >> 16) & 0xFF;
+		data->addr[pixel + 2] = (color >> 8) & 0xFF;
+		data->addr[pixel + 3] = (color) & 0xFF;
 	}
 	else if (data->endian == 0)
 	{
@@ -52,37 +52,52 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	}
 }
 
-void	bresenhams(t_mlx *mlx, t_line *line)
+static void	bresenham_init(t_bres *bresenham, t_line *line)
 {
-	int dx = abs(line->end.x - line->start.x);
-	int dy = -abs(line->end.y - line->start.y);
-	int sx = (line->start.x < line->end.x) ? 1 : -1;
-	int sy = (line->start.y < line->end.y) ? 1 : -1;
-	int err = dx + dy;
+	bresenham->dx = abs(line->end.x - line->start.x);
+	bresenham->dy = -abs(line->end.y - line->start.y);
+	if (line->start.x < line->end.x)
+		bresenham->step_x = 1;
+	else
+		bresenham->step_x = -1;
+	if (line->start.y < line->end.y)
+		bresenham->step_y = 1;
+	else
+		bresenham->step_y = -1;
+	bresenham->decision_parameter = bresenham->dx + bresenham->dy;
+	bresenham->x = line->start.x;
+	bresenham->y = line->start.y;
+}
 
-	int x = line->start.x;
-	int y = line->start.y;
-
+static void	bresenham_draw(t_mlx *mlx, t_line *line, t_bres *bresenham)
+{
 	while (1)
 	{
-		if (x >= 0 && y >= 0 && x < WIDTH && y < HEIGHT)
+		if (bresenham->x >= 0 && bresenham->y >= 0 && bresenham->x < WIDTH
+			&& bresenham->y < HEIGHT)
+			my_mlx_pixel_put(mlx->img, bresenham->x, bresenham->y,
+				line->start.rgb);
+		if (bresenham->x == (int)line->end.x
+			&& bresenham->y == (int)line->end.y)
+			break ;
+		bresenham->e2 = 2 * bresenham->decision_parameter;
+		if (bresenham->e2 >= bresenham->dy)
 		{
-			ft_printf("entrei\n");
-			my_mlx_pixel_put(mlx->img, x, y, line->start.rgb);
+			bresenham->decision_parameter += bresenham->dy;
+			bresenham->x += bresenham->step_x;
 		}
-		if (x == (int)line->end.x && y == (int)line->end.y)
-			break;
-		int e2 = 2 * err;
-		if (e2 >= dy)
+		if (bresenham->e2 <= bresenham->dx)
 		{
-			err += dy;
-			x += sx;
-		}
-		if (e2 <= dx)
-		{
-			err += dx;
-			y += sy;
+			bresenham->decision_parameter += bresenham->dx;
+			bresenham->y += bresenham->step_y;
 		}
 	}
-	mlx_loop(mlx->mlx);
+}
+
+void	bresenham(t_mlx *mlx, t_line *line)
+{
+	t_bres	bresenham;
+
+	bresenham_init(&bresenham, line);
+	bresenham_draw(mlx, line, &bresenham);
 }
