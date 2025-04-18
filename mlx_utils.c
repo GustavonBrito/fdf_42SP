ddfaa55b2366ee6126bc1c6728f422c535a8c090
GustavonBrito <gustavoserafio@gmail.com>
2025-04-18 19:45:52 -0300

    feat: mlx utils adapted on nomr

--- mlx_utils.c
+++ mlx_utils.c
@@ -6,20 +6,21 @@
 /*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 10:03:38 by gustavo-lin       #+#    #+#             */
-/*   Updated: 2025/04/18 10:35:13 by gustavo-lin      ###   ########.fr       */
+/*   Updated: 2025/04/18 19:45:36 by gustavo-lin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/fdf.h"
-#include "minilibx/mlx.h"
 #include "libft/headers/ft_printf.h"
-#include <signal.h>
+#include "minilibx/mlx.h"
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <signal.h>
 
-void close_window(t_mlx *mlx)
+void	close_window(t_mlx *mlx)
 {
-	int y;
+	int	y;
+
 	y = -1;
 	if (mlx->img->img)
 		mlx_destroy_image(mlx->mlx, mlx->img->img);
@@ -27,12 +28,14 @@ void close_window(t_mlx *mlx)
 		mlx_destroy_window(mlx->mlx, mlx->win);
 	if (mlx->map->coordinates)
 	{
-		while(++y < mlx->map->max_y)
+		while (++y < mlx->map->max_y)
 			free(mlx->map->coordinates[y]);
 		free(mlx->map->coordinates);
 		free(mlx->cam);
 		free(mlx->map);
 	}
+	if (mlx->img)
+		free(mlx->img);
 	if (mlx->mlx)
 	{
 		mlx_destroy_display(mlx->mlx);
@@ -42,11 +45,12 @@ void close_window(t_mlx *mlx)
 	exit(0);
 }
 
-int close_by_x(t_mlx *mlx)
+int	close_by_x(t_mlx *mlx)
 {
 	close_window(mlx);
 	return (0);
 }
+
 int	close_by_esc(int keycode, t_mlx *mlx)
 {
 	if (keycode == 65307)
