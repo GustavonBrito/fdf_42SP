5316cc9d14088e90f368e06f1537b63a9c2872e4
Axedassis <lssvgabriel@gmail.com>
2025-04-19 13:01:36 -0300

    fix(fd) close fd

--- map_parser.c
+++ map_parser.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:31:30 by gserafio          #+#    #+#             */
-/*   Updated: 2025/04/19 11:57:16 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/04/19 13:01:24 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ t_map	*init_parser(char *file_path)
 	map = center_map(map);
 	if (!map)
 		return (NULL);
+	close(fd);
 	return (map);
 }
 
